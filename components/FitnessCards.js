70602ef79f9f5ed566e78903d20ecad2370820b3
almukhametakbar <80895125+mukhtarov-daniyar@users.noreply.github.com>
2023-11-15 16:01:09 +0600

    versio n1

--- components/FitnessCards.js
+++ components/FitnessCards.js
@@ -1,28 +1,42 @@
+// Импорт необходимых компонентов из react-native и React
 import { StyleSheet, Text, View, Pressable, Image } from "react-native";
 import React from "react";
+// Импорт данных о фитнесе
 import fitness from "../data/fitness";
+// Импорт иконок из @expo/vector-icons
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+// Импорт хука useNavigation из @react-navigation/native для навигации
 import { useNavigation } from "@react-navigation/native";
 
+// Определение компонента FitnessCards
 const FitnessCards = () => {
+  // Присвоение данных о фитнесе из импортированного модуля
   const FitnessData = fitness;
+  // Использование хука навигации
   const navigation = useNavigation();
+
+  // Отрисовка компонента
   return (
+    // Контейнер View для отображения карточек
     <View>
+      // Итерация по данным о фитнесе и создание карточек для каждого элемента
       {FitnessData.map((item, key) => (
+        // Кнопка, которая при нажатии осуществляет навигацию
         <Pressable
-        onPress={() => navigation.navigate("Workout",{
-          image:item.image,
-          excersises:item.excersises,
-          id:item.id,
-        })}
+          onPress={() => navigation.navigate("Workout",{
+            image: item.image,
+            excersises: item.excersises,
+            id: item.id,
+          })}
           style={{ alignItems: "center", justifyContent: "center", margin: 10 }}
           key={key}
         >
+          // Отображение изображения карточки
           <Image
             style={{ width: "95%", height: 140, borderRadius: 7 }}
             source={{ uri: item.image }}
           />
+          // Текстовая метка на карточке
           <Text
             style={{
               position: "absolute",
@@ -35,8 +49,9 @@ const FitnessCards = () => {
           >
             {item.name}
           </Text>
+          // Иконка молнии на карточке
           <MaterialCommunityIcons
-            style={{ position: "absolute", color: "white", bottom: 15,left:20 }}
+            style={{ position: "absolute", color: "white", bottom: 15, left: 20 }}
             name="lightning-bolt"
             size={24}
             color="black"
@@ -47,6 +62,8 @@ const FitnessCards = () => {
   );
 };
 
+// Экспорт компонента FitnessCards
 export default FitnessCards;
 
+// Создание пользовательского стиля (в данном случае пустой)
 const styles = StyleSheet.create({});
